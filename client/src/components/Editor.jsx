bcd27092f2a8dce367faf642ef55388a8529c76f
Gaurav Singh Negi <gnegi897@gmail.com>
2024-04-30 20:22:56 +0530

    initial commit

---
+++ client/src/components/Editor.jsx
@@ -0,0 +1,12 @@
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+
+import {Box} from '@mui/material';
+
+const Editor = () => {
+  return (
+    <Box>Editor</Box>
+  )
+}
+
+export default Editor
